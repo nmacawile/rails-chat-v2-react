2359fb9fbc06f28e3f40afb2404360f2f8554084
Normar Macawile <nmacawile@gmail.com>
2024-05-15 14:42:20 +0000

    Add router

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,13 @@
 import "../stylesheets/App.css";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return <h1 className="text-2xl font-bold text-gray-700">React Rails Chat</h1>;
+  return (
+    <>
+      <h1 className="text-2xl font-bold text-gray-700">React Rails Chat</h1>
+      <Outlet />
+    </>
+  );
 }
 
 export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.jsx";
 import "./stylesheets/index.css";
+import Home from "./components/Home.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [{ path: "/", element: <Home /> }],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
