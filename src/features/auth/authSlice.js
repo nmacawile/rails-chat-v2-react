ad1120a0d3c59f7c787362254216658a2f1a9521
Normar Macawile <nmacawile@gmail.com>
2024-05-23 15:23:54 +0000

    Refactor to remove state mutation

--- src/features/auth/authSlice.js
+++ src/features/auth/authSlice.js
@@ -4,16 +4,11 @@ export const authSlice = createSlice({
   name: "auth",
   initialState: { auth_token: null, user: null, logged_in: false },
   reducers: {
-    logIn: (state, action) => {
-      const { auth_token, user } = action.payload;
-      state.auth_token = auth_token;
-      state.user = user;
-      state.logged_in = true;
+    logIn: (_, action) => {
+      return { ...action.payload, logged_in: true };
     },
-    logOut: (state) => {
-      state.auth_token = null;
-      state.user = null;
-      state.logged_in = false;
+    logOut: () => {
+      return { auth_token: null, user: null, logged_in: false };
     },
   },
 });
