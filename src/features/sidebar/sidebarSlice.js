de6e6d21241bab08b1b296886615975dac424492
Normar Macawile <nmacawile@gmail.com>
2024-06-29 10:17:49 +0000

    Implement querying in Redux

--- src/features/sidebar/sidebarSlice.js
+++ src/features/sidebar/sidebarSlice.js
@@ -2,14 +2,18 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const sidebarSlice = createSlice({
   name: "sidebar",
-  initialState: { searchMode: false },
+  initialState: { searchMode: false, query: "" },
   reducers: {
     setSearchMode: (state, action) => ({
       ...state,
       searchMode: action.payload,
     }),
+    setQuery: (state, action) => ({
+      ...state,
+      query: action.payload,
+    }),
   },
 });
 
-export const { setSearchMode } = sidebarSlice.actions;
+export const { setSearchMode, setQuery } = sidebarSlice.actions;
 export default sidebarSlice.reducer;
--- src/features/sidebar/sidebarSlice.test.js
+++ src/features/sidebar/sidebarSlice.test.js
@@ -1,11 +1,22 @@
 import { describe } from "vitest";
-import sidebarSlice, { setSearchMode } from "./sidebarSlice";
+import sidebarSlice, { setQuery, setSearchMode } from "./sidebarSlice";
 
 describe("sidebarSlice", () => {
   describe("sidebar reducer", () => {
     it("changes the state of searchMode", () => {
-      const newState = sidebarSlice({ searchMode: false }, setSearchMode(true));
-      expect(newState).toEqual({ searchMode: true });
+      const newState = sidebarSlice(
+        { searchMode: false, query: "" },
+        setSearchMode(true)
+      );
+      expect(newState).toEqual({ searchMode: true, query: "" });
+    });
+
+    it("changes the state of query", () => {
+      const newState = sidebarSlice(
+        { searchMode: true, query: "" },
+        setQuery("test")
+      );
+      expect(newState).toEqual({ searchMode: true, query: "test" });
     });
   });
 });
