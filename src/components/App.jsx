370bc6bb237a4d30c8908e54e2a889ee17dfa003
Normar Macawile <nmacawile@gmail.com>
2024-09-16 16:27:43 +0000

    Automatically check token validity on startup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,18 +2,29 @@ import "../stylesheets/App.css";
 import { Outlet, Navigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { WebSocketProvider } from "../contexts/WebSocketContext";
+import { logoutThunk } from "../thunks/authThunks";
+import { useDispatch } from "react-redux";
 
 function App() {
   const auth = useSelector((state) => state.auth);
+  const dispatch = useDispatch();
+
+  const tokenExpired = () => Date.now() > auth.exp * 1000;
+  const validateAuth = () => auth.logged_in && !tokenExpired();
+
+  const logOut = () => {
+    dispatch(logoutThunk());
+    return <Navigate to="/auth/login" />;
+  };
 
   return (
     <div className="p-4 h-full">
-      {auth.logged_in ? (
+      {validateAuth() ? (
         <WebSocketProvider>
           <Outlet />
         </WebSocketProvider>
       ) : (
-        <Navigate to="/auth/login" />
+        logOut()
       )}
     </div>
   );
--- src/components/App.test.jsx
+++ src/components/App.test.jsx
@@ -1,11 +1,18 @@
-import { describe, it, expect } from "vitest";
-import { render, screen } from "@testing-library/react";
+import { describe, it, expect, vi } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
 import App from "./App.jsx";
 import configureMockStore from "redux-mock-store";
 import { MemoryRouter, Route, Routes } from "react-router-dom";
 import { Provider } from "react-redux";
+import { authUserFixture } from "../../tests/fixtures/usersFixture";
+import { thunk } from "redux-thunk";
+import { logoutThunk } from "../thunks/authThunks";
+import { act } from "react-dom/test-utils";
 
-const mockStore = configureMockStore([]);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+vi.mock("../thunks/authThunks");
 
 const renderWithCustomStore = (store) => {
   render(
@@ -22,15 +29,38 @@ const renderWithCustomStore = (store) => {
   );
 };
 
+const expiredTimestamp = new Date("1 October 2024").getTime() / 1000;
+const validTimestamp = new Date("1 January 2025").getTime() / 1000;
+
+const auth = { user: authUserFixture, logged_in: true, exp: validTimestamp };
+
 describe("App Component", () => {
   it("redirects to Login page when unauthenticated", () => {
-    const store = mockStore({ auth: { logged_in: false } });
+    const store = mockStore({ auth: { ...auth, logged_in: false } });
+    store.dispatch = vi.fn();
     renderWithCustomStore(store);
     expect(screen.getByText("Login Page")).toBeInTheDocument();
   });
 
+  it("Logs out and redirects user to Login page when token expires", () => {
+    const store = mockStore({
+      auth: { ...auth, exp: expiredTimestamp },
+    });
+    logoutThunk = vi.fn();
+    store.dispatch = vi.fn();
+
+    const mockDate = new Date("1 December 2024").getTime();
+    vi.spyOn(Date, "now").mockImplementation(() => mockDate);
+
+    act(() => renderWithCustomStore(store));
+
+    expect(store.dispatch).toHaveBeenCalled();
+    expect(logoutThunk).toHaveBeenCalled();
+    expect(screen.getByText("Login Page")).toBeInTheDocument();
+  });
+
   it("remains on the page when authenticated", () => {
-    const store = mockStore({ auth: { logged_in: true } });
+    const store = mockStore({ auth });
     renderWithCustomStore(store);
     expect(screen.getByText("Welcome")).toBeInTheDocument();
   });
