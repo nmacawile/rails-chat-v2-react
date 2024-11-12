82a7966df7b7e9420e668441ad01a404f4390c00
Normar Macawile <nmacawile@gmail.com>
2024-11-12 16:41:44 +0000

    Display logged-in user's handle

--- src/components/sidebar/UserInfoPanel.jsx
+++ src/components/sidebar/UserInfoPanel.jsx
@@ -1,17 +1,16 @@
 import { useSelector } from "react-redux";
 
 export function UserInfoPanel() {
-  const auth_user = useSelector((state) => state.auth.user);
+  const { handle, full_name } = useSelector((state) => state.auth.user);
 
   return (
-    <>
-      <div>
-        <div className="bg-purple-400 h-10 w-10 rounded-full"></div>
+    <div className="flex flex-row">
+      <div className="bg-purple-400 h-10 w-10 rounded-full"></div>
+      <div className="ms-2.5 flex flex-col">
+        <span className="leading-none text-white font-semibold">{full_name}</span>
+        <span className="user-handle text-gray-400">{handle}</span>
       </div>
-      <span className="ms-2.5 leading-none text-white">
-        {auth_user.full_name}
-      </span>
-    </>
+    </div>
   );
 }
 
